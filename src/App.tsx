67016b641230f109338ae963d0847060c2c9706d
Aditya Karnam <akarnam37@gmail.com>
2021-07-06 17:36:38 -0500

    feat: instagram api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Appbar } from 'react-native-paper';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ImageBackground } from 'react-native';
 import ReceiveSharingIntent from 'react-native-receive-sharing-intent';
+import InstagramApi from 'simple-instagram-api';
 import appJSON from '../app.json';
+
+const INSTAGRAM_BASE_URL = 'https://www.instagram.com/p/';
 
 export default function App() {
   const _handleSearch = () => console.log('Searching');
   const _handleMore = () => console.log('Shown more');
+
+  const [imageSource, setImageSource] = React.useState('')
   React.useEffect(() => {
     ReceiveSharingIntent.getReceivedFiles((data:any)=> {
       console.log(data);
+      const { weblink } = data[0];
+      // Exmaple URL Split for Share URL
+      // https://www.instagram.com/p/CQ9MJFEA6xX/?utm_medium=share_sheet
+      // ["https:", "", "www.instagram.com", "p", "CQ9MJFEA6xX", "?utm_medium=share_sheet"]
+      const shortCode = weblink.split('/')[4];
+      InstagramApi.get(shortCode).then((result) => {
+        setImageSource(result.url);
+      });
     },
     (err:any)=>{
       console.log(err);
@@ -33,6 +46,9 @@ export default function App() {
             <Text>Open up App.tsx to start working on your app! Yeah man its so good. Yes it might be working. </Text>
             <StatusBar style="auto" />
         </View>
+        <ImageBackground source={imageSource}>
+
+        </ImageBackground>
       </SafeAreaView>
   );
 }
